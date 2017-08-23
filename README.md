b266b058220d53ad506cab1d0d46b0d3df9b5345
Sam Hamm <samhamm@gmail.com>
2017-08-23 08:51:46 -0700

    publishing lab

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# 08-sql-intro-and-postgres
-Class 8 lab assignment will be published here
+![CF](https://i.imgur.com/7v5ASc8.png)  Lab 08: SQL and Postgres
+=======
+[Code of Conduct](https://github.com/codefellows/code-of-conduct)
+
+## Submission Instructions
+When you are finished with lab, follow these steps to submit your work. Create one Pull Request (aka: "PR") from your Forked repo to the CF repo with your changes, and you'll each submit that same PR link in Canvas.
+
+1. Ensure that all your local changes are committed, and pushed to your origin repo.
+1. Visit the origin repo on github.com, and ensure that all of your completed work has been merged to master via Pull Requests within your repo.
+1. Create a new PR from your Fork to the CF repo and ensure the branches look correct.
+1. Fill in the template based on the text box prompts:
+  1. Write a good descriptive summary of your changes:
+    1. Be sure to include how much time you spent on it, and who you worked with.
+    1. Briefly reflect on and summarize your process.
+1. When you create the PR, it will have a unique URL. Copy this link, share with your partner, and paste it into the assignment submission form in Canvas. Both the driver and the navigator will submit the same PR link.
+---
+
+## Learning Objectives
+
+- Understand the basic concepts of a database
+- Effectively use basic SQL commands to create, read, update, and delete rows from a table
+
+---
+
+## Resources  
+[SQL Syntax Cheatsheet](cheatsheets/sql.md)
+[PostgreSQL Shell Cheatsheet](cheatsheets/postgress-shell.md)
+
+---
+
+## Feature Tasks  
+
+1. Complete the TODO items in `server.js` to ensure that your server file is properly configured
+
+2. Study each of the new routes in your `server.js` file by examining the SQL statements and any associated data being handed through the request.
+
+3. For each of the COMMENT items in `server.js`, provide a brief description of what that function immediately below is doing. Be sure to indicate, where applicable, details such as:
+  - What number(s) of the full-stack-diagram.png image is this part of the code interacting with?
+  - Which method of `article.js` is interacting with this particular piece of `server.js`?
+  - What part of ***CRUD*** is being enacted/managed by this particular piece of code?
+  - As applicable, an additional sentence or two of explanation about what the code is doing, what NPM packages are involved, etc. The goal is to demonstrate that you understand what is going on in the code without glossing over details, but also without writing a novel about it.
+
+#### Stretch Goals
+
+* You may test each of these routes by utilizing the corresponding Article prototype methods in the `article.js` file.
+* For example, the `app.post()` route in `server.js` corresponds to the `Article.prototype.insertRecord()` method in `article.js`. You can create a new Article object and call that method on it from the browser console. You can then check your Postgres DB to confirm that it exists in the DB. It will also render to the page upon refreshing the browser.
+```javascript
+  // In the browser console
+  let myArticle = new Article({title:'Flibbity goes Jibbiting', author:'Flibbity Jibbit', authorUrl:'flibbity.jibbit.com', category:'jibbits', publishedOn:'01-01-2217', body:'Flibbity Jibbit and the Key Keeper'});
+
+  myArticle.insertRecord();
+```
+
+Refresh the page and you will see the new article at the top of the blog, plus the relevant information populated into the author and category filters.
+
+You can test/verify each of the routes through console commands like this.
+
+---
+
+## Rubric  
+
+Criteria | Pts
+---|---
+Meets all Assignment Reqs | 6
+Uses idiomatic code style | 3
+Follows proper Git workflow | 1
+**Total** | **10**
---
+++ starter-code/server.js
@@ -0,0 +1,193 @@
+'use strict';
+
+// TODO: Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
+const fs = require('fs');
+const express = require('express');
+
+// REVIEW: Require in body-parser for post requests in our server. If you want to know more about what this does, read the docs!
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// TODO: Complete the connection string for the url that will connect to your local postgres database
+// Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
+// Your url may require that it's composed of additional information including user and password
+// const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
+const conString = 'postgres://localhost:5432';
+
+// TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
+//       This is how it knows the URL and, for Windows and Linux users, our username and password for our
+//       database when client.connect is called on line 26. Thus, we need to pass our conString into our
+//       pg.Client() call.
+const client = new pg.Client('something needs to go here... read the instructions above!');
+
+// REVIEW: Use the client object to connect to our DB.
+client.connect();
+
+
+// REVIEW: Install the middleware plugins so that our app is aware and can use the body-parser module
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+
+// REVIEW: Routes for requesting HTML resources
+app.get('/new', function(request, response) {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  response.sendFile('new.html', {root: './public'});
+});
+
+
+// REVIEW: Routes for making API calls to use CRUD Operations on our database
+app.get('/articles', function(request, response) {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query('SELECT * FROM articles')
+  .then(function(result) {
+    response.send(result.rows);
+  })
+  .catch(function(err) {
+    console.error(err)
+  })
+});
+
+app.post('/articles', function(request, response) {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query(
+    `INSERT INTO
+    articles(title, author, "authorUrl", category, "publishedOn", body)
+    VALUES ($1, $2, $3, $4, $5, $6);
+    `,
+    [
+      request.body.title,
+      request.body.author,
+      request.body.authorUrl,
+      request.body.category,
+      request.body.publishedOn,
+      request.body.body
+    ]
+  )
+  .then(function() {
+    response.send('insert complete')
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+app.put('/articles/:id', function(request, response) {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query(
+    `UPDATE articles
+    SET
+      title=$1, author=$2, "authorUrl"=$3, category=$4, "publishedOn"=$5, body=$6
+    WHERE article_id=$7;
+    `,
+    [
+      request.body.title,
+      request.body.author,
+      request.body.authorUrl,
+      request.body.category,
+      request.body.publishedOn,
+      request.body.body,
+      request.params.id
+    ]
+  )
+  .then(function() {
+    response.send('update complete')
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+app.delete('/articles/:id', function(request, response) {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query(
+    `DELETE FROM articles WHERE article_id=$1;`,
+    [request.params.id]
+  )
+  .then(function() {
+    response.send('Delete complete')
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+app.delete('/articles', function(request, response) {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query(
+    'DELETE FROM articles;'
+  )
+  .then(function() {
+    response.send('Delete complete')
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+// COMMENT: What is this function invocation doing?
+// Put your response here...
+loadDB();
+
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}!`);
+});
+
+
+//////// ** DATABASE LOADER ** ////////
+////////////////////////////////////////
+function loadArticles() {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query('SELECT COUNT(*) FROM articles')
+  .then(result => {
+    // REVIEW: result.rows is an array of objects that Postgres returns as a response to a query.
+    //         If there is nothing on the table, then result.rows[0] will be undefined, which will
+    //         make count undefined. parseInt(undefined) returns NaN. !NaN evaluates to true.
+    //         Therefore, if there is nothing on the table, line 151 will evaluate to true and
+    //         enter into the code block.
+    if(!parseInt(result.rows[0].count)) {
+      fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
+        JSON.parse(fd.toString()).forEach(ele => {
+          client.query(`
+            INSERT INTO
+            articles(title, author, "authorUrl", category, "publishedOn", body)
+            VALUES ($1, $2, $3, $4, $5, $6);
+          `,
+            [ele.title, ele.author, ele.authorUrl, ele.category, ele.publishedOn, ele.body]
+          )
+        })
+      })
+    }
+  })
+}
+
+function loadDB() {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // Put your response here...
+  client.query(`
+    CREATE TABLE IF NOT EXISTS articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      author VARCHAR(255) NOT NULL,
+      "authorUrl" VARCHAR (255),
+      category VARCHAR(20),
+      "publishedOn" DATE,
+      body TEXT NOT NULL);`
+    )
+    .then(function() {
+      loadArticles();
+    })
+    .catch(function(err) {
+      console.error(err);
+    }
+  );
+}
